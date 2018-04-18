2bf0d2de6c97180d9e611e7e7b8e6466a4c6a0f7
mlescano <Mack.Lescano_avantica@scotiabank.com.pe>
2018-04-18 17:53:09 -0500

    Migration to 2.0.1.RELEASE spring boot

--- src/main/java/main/MainApplication.java
+++ src/main/java/main/MainApplication.java
@@ -3,6 +3,7 @@ package main;
 import child1.ChildContext1;
 import child2.ChildContext2;
 import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import root.RootContext;
@@ -13,11 +14,11 @@ public class MainApplication {
                 new SpringApplicationBuilder()
                         .parent(RootContext.class)
                         .child(ChildContext1.class)
-                        .sibling(ChildContext2.class);
+                        .sibling(ChildContext2.class).web(WebApplicationType.NONE).bannerMode(Banner.Mode.OFF);
 
-        ConfigurableApplicationContext applicationContext  = appBuilder.run();
-        applicationContext.setId("child2");
-        ((ConfigurableApplicationContext)applicationContext.getParent()).setId("root");
+        ConfigurableApplicationContext applicationContextChild2  = appBuilder.run();
+        applicationContextChild2.setId("child2");
+        /*((ConfigurableApplicationContext)applicationContext.getParent()).setId("root");*/
 //
 //        for (String bdn: applicationContext.getBeanDefinitionNames()) {
 //            System.out.println("bdn = " + bdn);
